86ebf13050a1827446750fddeb5fd658234a5caa
Asilvazavala <asilvazavala@gmail.com>
2023-04-24 14:47:35 -0600

    fourth commit

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,9 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const {
+  ACCESS_FRONT
+} = process.env;
 
 require('./db.js');
 
@@ -15,7 +18,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pokemon-world.onrender.com'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', ACCESS_FRONT); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
